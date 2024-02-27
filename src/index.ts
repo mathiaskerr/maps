630d97bcaa98d86841332e8120b03cac67fecf06
Mathias <114953633+mathiaskerr@users.noreply.github.com>
2024-02-27 15:52:12 +0000

    display google map

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
-import {User} from './User';
-import { Company } from './Company';
+// import {User} from './User';
+// import { Company } from './Company';
 
-const user = new User();
-console.log(user);
 
-const company = new Company();
-console.log(company);
+new google.maps.Map(document.getElementById('map') as HTMLElement,{
+    zoom: 3,
+    center: {
+        lat: 0,
+        lng: 0
+    }
+});
+

cd7849403154a7c528030d1bd9e3afa42a321386
Mathias <114953633+mathiaskerr@users.noreply.github.com>
2024-03-05 18:08:09 +0000

    finishing map app

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,11 +1,12 @@
 // import { User } from "./User";
 // import { Company } from "./Company";
 
-interface Mappable {
+export interface Mappable {
     location: {
         lat: number;
         lng: number;
     }
+    markerContent():string;
 }
 
 export class CustomMap {
@@ -13,7 +14,7 @@ export class CustomMap {
 
     constructor(divId: string){
         this.googleMap = new google.maps.Map(document.getElementById(divId) as HTMLElement,{
-            zoom: 3,
+            zoom: 2,
             center: {
                 lat: 0,
                 lng: 0
@@ -22,12 +23,19 @@ export class CustomMap {
     }
 
     addMarker(mappable: Mappable): void{
-        new google.maps.Marker({
+        const marker = new google.maps.Marker({
             map: this.googleMap,
             position: {
                 lat: mappable.location.lat,
                 lng: mappable.location.lng
             }
+        });
+
+        marker.addListener('click', () => {
+            const infoWindow = new google.maps.InfoWindow({
+                content: mappable.markerContent()
+            });
+            infoWindow.open(this.googleMap, marker);
         });
     }
 }
